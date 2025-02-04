599bf976283c59202436cb27fc16fce0b5d20c31
underbreathing <artem-sheverdyaev@mail.ru>
2025-02-05 01:55:07 +0300

    cleaning

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -38,6 +38,4 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
